d1d34d83f89b5e35c8d99482f1420728cb36b6f6
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2020-06-04 21:25:56 +0700

    Hide pycache and add .gitignore

--- features.py
+++ features.py
@@ -2,7 +2,7 @@ from python_speech_features import mfcc
 import numpy as np
 
 def get_feature(fs, signal):
-    mfcc_feature = mfcc(signal, fs)
+    mfcc_feature = mfcc(signal, fs, nfft=1104)
     if len(mfcc_feature) == 0:
         print >> sys.stderr, "ERROR.. failed to extract mfcc feature:", len(signal)
     return mfcc_feature
