9eaa16e14160f3872cf3ca64d768201ea542a33f
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-06-05 22:02:02 +0700

    Change nfft to 4096

--- features.py
+++ features.py
@@ -2,7 +2,9 @@ from python_speech_features import mfcc
 import numpy as np
 
 def get_feature(fs, signal):
-    mfcc_feature = mfcc(signal, fs, nfft=4096*3)
+    mfcc_feature = mfcc(signal, fs, nfft=4096)
+    #mfcc_feature = mfcc(signal, fs)
+    #print(fs)
     if len(mfcc_feature) == 0:
         print >> sys.stderr, "ERROR.. failed to extract mfcc feature:", len(signal)
     return mfcc_feature
